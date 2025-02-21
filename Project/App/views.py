e5ceec5da12e3502747709cc00e2ac5c665cc0c9
bonabigum <iiiooovii@gmail.com>
2025-02-21 19:39:22 +0800

    Testing env

--- Project/App/views.py
+++ Project/App/views.py
@@ -1,6 +1,13 @@
+import os
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def test_env(): #just testing env
+    secret_test = os.getenv("SECRET_TEST", "Not Found")
+    print("Environment Variable:", secret_test)
+test_env()
 
 def index(request):
     template = "main/index.html"
