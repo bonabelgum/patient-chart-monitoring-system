368834e6906b7d2054baaa8727af00b748addd95
bonabelgum <iiiooovii@gmail.com>
2025-02-26 00:05:33 +0800

    fix CSRF link

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv #env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= ["patient-chart-monitoring-system-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS= ["https://patient-chart-monitoring-system-production.up.railway.app"]
 
 
 # Quick-start development settings - unsuitable for production
